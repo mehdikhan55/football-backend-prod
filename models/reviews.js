47a84d84699f7e7b8e4ff31d191fdf37f107314b
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-10-28 18:14:31 +0500

    updated

--- models/reviews.js
+++ models/reviews.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
     description: {
         type: String,
         required: true,
